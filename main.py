2000c82fea762d9ae49240284bc3004b4ccedeb8
TeTRisyaka <tetriskamana@gmail.com>
2024-11-19 16:07:44 +0300

    Добавил возможность ввода исходных данных в консоль

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 
 
-fileR = open("accounts.txt", "r")
-fileW = open("accounts_imap.txt", "w+")
-content = fileR.read()
-psw = content.split()
-for item in psw:
-    spl = item.split(":")
-    fileW.write(spl[0] + ':' + spl[1] + ':' + spl[1] + '\n')
-fileR.close()
-fileW.close()
+
+# True, если вы используете текстовый файл, False, если хотите вставить скопированный текст
+readFile = False
+
+
+if readFile == True:
+    fileR = open("accounts.txt", "r")
+    fileW = open("accounts_imap.txt", "w+")
+    content = fileR.read()
+    psw = content.split()
+    for item in psw:
+        spl = item.split(":")
+        fileW.write(spl[0] + ':' + spl[1] + ':' + spl[1] + '\n')
+    fileR.close()
+    fileW.close()
+else:
+    fileR = ""
+    print("Введите данные в консоль в формате почта:пароль (без разрывов строк)")
+    while True:
+        text = input()
+        if text:
+            fileR += text + " "
+        else:
+            break
+    fileW = open("accounts_imap_test.txt", "w+")
+    psw = fileR.split()
+    for item in psw:
+        spl = item.split(":")
+        fileW.write(spl[0] + ':' + spl[1] + ':' + spl[1] + '\n')
+    fileW.close()
