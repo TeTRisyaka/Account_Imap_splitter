7035b839950e8fc85dfb46f6d4dff15a27d12991
TeTRisyaka <tetriskamana@gmail.com>
2024-11-19 16:40:39 +0300

    Добавил возможность отображения результатов в консоль и возможность не создавать выходной файл

--- main.py
+++ main.py
@@ -2,24 +2,35 @@
 
 
 # True, если вы используете текстовый файл, False, если хотите вставить скопированный текст
-readFromFile = True
+readFromFile = False
 
 
 # True, если вы хотите очистить свои данные от лишнего imap пароля
 DeleteImapPsw = False
 
+# True, если вы хотите чтобы результат работы был дополнительно написан в консоли
+PrintResult = True
+
+# True, если вы хотите чтобы результат работы был в файле
+WriteResult = False
+
 
 if DeleteImapPsw == False:
     if readFromFile == True:
         fileR = open("accounts.txt", "r")
-        fileW = open("accounts_imap.txt", "w+")
+        if WriteResult == True:
+            fileW = open("accounts_imap.txt", "w+")
         content = fileR.read()
         psw = content.split()
         for item in psw:
             spl = item.split(":")
-            fileW.write(spl[0] + ':' + spl[1] + ':' + spl[1] + '\n')
+            if WriteResult == True:
+                fileW.write(spl[0] + ':' + spl[1] + ':' + spl[1] + '\n')
+            if PrintResult == True:
+                print(spl[0] + ':' + spl[1] + ':' + spl[1] + '\n')
         fileR.close()
-        fileW.close()
+        if WriteResult == True:
+            fileW.close()
     else:
         fileR = ""
         print("Введите данные в консоль в формате почта:пароль (без разрывов строк)")
@@ -29,23 +40,33 @@ if DeleteImapPsw == False:
                 fileR += text + " "
             else:
                 break
-        fileW = open("accounts_imap_test.txt", "w+")
+        if WriteResult == True:
+            fileW = open("accounts_imap_test.txt", "w+")
         psw = fileR.split()
         for item in psw:
             spl = item.split(":")
-            fileW.write(spl[0] + ':' + spl[1] + ':' + spl[1] + '\n')
-        fileW.close()
+            if WriteResult == True:
+                fileW.write(spl[0] + ':' + spl[1] + ':' + spl[1] + '\n')
+            if PrintResult == True:
+                print(spl[0] + ':' + spl[1] + ':' + spl[1])
+        if WriteResult == True:
+            fileW.close()
 else:
     if readFromFile == True:
         fileR = open("accounts.txt", "r")
-        fileW = open("accounts_without_imap.txt", "w+")
+        if WriteResult == True:
+            fileW = open("accounts_without_imap.txt", "w+")
         content = fileR.read()
         psw = content.split()
         for item in psw:
             spl = item.split(":")
-            fileW.write(spl[0] + ':' + spl[1] + '\n')
+            if WriteResult == True:
+                fileW.write(spl[0] + ':' + spl[1] + '\n')
+            if PrintResult == True:
+                print(spl[0] + ':' + spl[1] + '\n')
         fileR.close()
-        fileW.close()
+        if WriteResult == True:
+            fileW.close()
     else:
         fileR = ""
         print("Введите данные в консоль в формате почта:пароль (без разрывов строк)")
@@ -55,9 +76,14 @@ else:
                 fileR += text + " "
             else:
                 break
-        fileW = open("accounts_without_imap.txt", "w+")
+        if WriteResult == True:
+            fileW = open("accounts_without_imap.txt", "w+")
         psw = fileR.split()
         for item in psw:
             spl = item.split(":")
-            fileW.write(spl[0] + ':' + spl[1] + '\n')
-        fileW.close()
+            if WriteResult == True:
+                fileW.write(spl[0] + ':' + spl[1] + '\n')
+            if PrintResult == True:
+                print(spl[0] + ':' + spl[1] + '\n')
+        if WriteResult == True:
+            fileW.close()
